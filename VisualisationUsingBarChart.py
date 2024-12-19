de0fca4f9f76432df33fb994799556d668481ce5
avya01 <avyasurnam@gmail.com>
2024-12-19 15:41:49 +0400

    first commit

---
+++ VisualisationUsingBarChart.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = {
+    "Day" : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    "Birth_Rate" : [10, 12, 16, 9, 7]
+}
+
+df_July = pd.DataFrame(data)
+ 
+plt.bar(df_July.Day, df_July.Birth_Rate, width=0.5, color="lightblue", ec="green")
+plt.xlabel("Day")
+plt.ylabel("Birth Rate")
+plt.title("Birth Rate VS Day for July")
+plt.show()
+
+data2 = {
+    "Day" : ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+    "Birth_Rate" : [5, 18, 4, 7, 9]
+}
+
+df_August = pd.DataFrame(data2)
+
+plt.bar(df_August.Day, df_August.Birth_Rate, width=0.5, color="lightblue", ec="green")
+plt.xlabel("Day")
+plt.ylabel("Birth Rate")
+plt.title("Birth Rate VS Day for August")
+plt.show()
